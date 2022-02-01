d383e68f31a9222762ec556ab5aeae503f669468
cody elm <codyelm@gmail.com>
2022-02-01 07:22:40 -0800

    final commit

--- script.js
+++ script.js
@@ -1,12 +1,76 @@
-var today = moment();
-$("#1a").text(today.format("MMM Do, YYYY"));
+$(function () {});
 
-var dayWeek = today.format("[Today is] dddd")
-$("#2a").text(dayWeek);
+var today = moment().format("dddd, MMMM Do");
+
+var now = moment().format("H A");
 
 
-var reformatDate = moment("11/3/20", "MM-DD-YY").format("dddd, MMMM Do YYYY, h:mm:ss a");
-$("#3a").text(reformatDate);
+var Workday = [
+	
+  { time: "9 AM", event: "" },
+  { time: "10 AM", event: "" },
+  { time: "11 AM", event: "" },
+  { time: "12 PM", event: "" },
+  { time: "1 PM", event: "" },
+  { time: "2 PM", event: "" },
+  { time: "3 PM", event: "" },
+  { time: "4 PM", event: "" },
+  { time: "5 PM", event: "" },
+];
 
-var weekNum = today.format("w");
-var takeOut;
+
+var Events = JSON.parse(localStorage.getItem("workDay"));
+if (Events) {
+  Workday = Events;
+}
+
+
+$("#current").text(today);
+
+Workday.forEach(function(timeBlock, index) {
+	var timeLabel = timeBlock.time;
+	var blockColor = colorRow(timeLabel);
+	var row =
+		'<div class="time-block" id="' +
+		index +
+		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+		timeLabel +
+		'</div><textarea class="form-control ' +
+		blockColor +
+		'">' +
+		timeBlock.event +
+		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+
+
+	$(".container-fluid").append(row);
+});
+
+function colorRow(time) {
+	var planNow = moment(now, "H A");
+	var planEntry = moment(time, "H A");
+	if (planNow.isBefore(planEntry) === true) {
+		return "future";
+	} else if (planNow.isAfter(planEntry) === true) {
+		return "past";
+	} else {
+		return "present";
+	}
+}
+
+
+$(".saveBtn").on("click", function() {
+	var blockID = parseInt(
+		$(this)
+			.closest(".time-block")
+			.attr("id")
+	);
+	var userEntry = $.trim(
+		$(this)
+			.parent()
+			.siblings("textarea")
+			.val()
+	);
+	Workday[blockID].event = userEntry;
+
+	localStorage.setItem("workDay", JSON.stringify(Workday));
+});
